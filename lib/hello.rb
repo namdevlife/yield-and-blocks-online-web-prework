c916a49d0f93b083123049300dd8f2c8d66a1247
NAM NGUYEN <namdevlife@gmail.com>
2019-05-28 04:29:20 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,13 +6,6 @@ def hello_t(array)
     i = i + 1
   end
   
-  ["Tim", "Tom", "Jim"]
-hello_t(["Tim", "Tom", "Jim"]) do |name|
-  if name.start_with?("T")
-    puts "Hi, #{name}"
-  end
-end
- 
   array
 end
 
